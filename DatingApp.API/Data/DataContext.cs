87eb96a8609abb6d56d5652b6bcf8e46b0abaa4d
Gustavo Gabriel Gonzalez Somerstein <gggonzalez@fassil.com.bo>
2019-05-18 12:51:35 -0400

    Commit inicial

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; } //Values va a ser el nombre de la tabla
+    }
+}
